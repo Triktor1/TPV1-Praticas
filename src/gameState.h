5a93802d650ad40c5a17fff6429f4393d77156ee
Triktor <yosoyvictor06@gmail.com>
2025-11-25 17:37:36 +0100

    clases nuevas

--- src/gameState.h
+++ src/gameState.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <list>
+#include <functional> 
+
+class GameObject;
+class EventHandler;
 class GameState
 {
+protected:
+	using DelayedCallback = std::function<void()>;
+	list<GameObject*> gameObjects;
+	list<EventHandler*> eventHandlers;
+	list<DelayedCallback> delayedCallbacks;
 };
 
